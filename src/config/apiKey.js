a4bdcce3b7c986a8e18572db6db27520ee15d58a
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-05-06 11:38:56 -0300

    feat: add prints

--- src/config/apiKey.js
+++ src/config/apiKey.js
@@ -1,14 +1,18 @@
+require("dotenv").config();
+
 const apiKeyMiddleware = (req, res, next) => {
     const clientKey = req.headers['x-api-key'];
     const serverKey = process.env.API_KEY;
   
-    if (!clientKey || clientKey !== serverKey) {
-      return res.status(403).json({ error: 'Chave da API não fornecida!' });
+    if (!clientKey) {
+      return res.status(403).json({ error: 'Chave de API ausente' });
     }
-    if(clientKey !== serverKey)
-        return res.status(403).json({error: 'Chave da API incorreta! Sem autorização'})
-  
+    if (clientKey !== serverKey) {
+        return res.status(403).json({ error: 'Chave de API invalida' });
+    }
+   
     next();
   };
   
   module.exports = apiKeyMiddleware;
+  
